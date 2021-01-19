b09de42ace3b17483e7ab0b7f770d51bcfc9dd95
sunway <sunwayforever@gmail.com>
2021-01-20 00:05:19 +0800

    nil

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ln -s ~/Gitbox/mhp3/SAVEDATA ~/.config/ppsspp/PSP/SAVEDATA
+ln -s ~/Gitbox/mhp3/SAVEDATA ~/.config/ppsspp/PSP/
 mkdir ~/.config/ppsspp/PSP/Cheats
 cp NPJB40001.ini ~/.config/ppsspp/PSP/Cheats
